1b0c649cac1ee7176620d07405c70dc4583fc02d
Arshdeep Singh <arsh94deep@gmail.com>
2018-11-30 11:23:17 +0530

    issue

--- XcodeServer/ViewController.swift
+++ XcodeServer/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
 
     func returnString() -> String{
-        return ""
+        return nil
     }
     
     override func didReceiveMemoryWarning() {
