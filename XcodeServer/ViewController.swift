be4cb4b4325cc6506ac0f734aca369a8b0179fcf
K P Singh <kpsingh@kanwarpal-iphone.local>
2018-11-30 10:32:02 +0530

    error check

--- XcodeServer/ViewController.swift
+++ XcodeServer/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        returnString()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func returnString() -> String{
+        return nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
